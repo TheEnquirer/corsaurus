9f1ddde91126ab84ed55fde6898f0ecf3ac0a5df
TheEnquirer <huxmarv@nuevaschool.org>
2021-04-05 21:27:15 -0700

    breakpoint and tests

--- tests.py
+++ tests.py
@@ -9,8 +9,16 @@ wv_model = word2vec.Word2Vec.load('./data/1billion_word_vectors/1billion_word_ve
 wordvec = wv_model.wv
 del wv_model
 
+breakpoint()
 
-a = wordvec.most_similar(positive=['king', 'woman'], negative=['man'])
-print(a)
+
+# Cosmul seems to work better
+
+# a = wordvec.most_similar(positive=['king', 'woman'], negative=['man'], topn=3)
+# print(a)
+
+
+# wordvec.most_similar_cosmul(positive=['sports', 'digital'], negative=['exercise'], topn=10)
+# wordvec.most_similar(positive=['king', 'woman'], negative=['man'], topn=10)
 
 
