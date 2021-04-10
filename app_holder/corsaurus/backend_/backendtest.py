4eee13ca835f408d51d9116217dfc06527129662
TheEnquirer <huxmarv@nuevaschool.org>
2021-04-09 22:48:32 -0700

    LADIES AND GENTELMEN, WE GOT THEM

--- app_holder/corsaurus/backend_/backendtest.py
+++ app_holder/corsaurus/backend_/backendtest.py
@@ -1,15 +1,27 @@
-# pylint: disable=no-member
 import time
-from flask import Flask
+import json
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/time')
+@app.route('/time', methods=['PUT'])
 @cross_origin()
 def get_current_time():
     app.logger.info('hereee')
-    return {'time': time.time()}
+    # content = json.loads(request.data)
+    data = request.data
+    data = data.decode("utf-8")
+    data = jsonify(data)
+    app.logger0info(data)
+    # return {'time': time.time()}
+    return data
 
-app.logger.info('sss')
+@app.route('/query', methods=['PUT'])
+@cross_origin()
+def query():
+    app.logger.info('sss')
+    return {'payload': 'ahahhahaha'}
+
+
