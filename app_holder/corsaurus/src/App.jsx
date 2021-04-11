b0f6b67dfdaa10fa6712489ac97f8429a5512a24
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2021-04-10 22:09:38 -0700

    Fixed formatting :D

--- app_holder/corsaurus/src/App.jsx
+++ app_holder/corsaurus/src/App.jsx
@@ -4,36 +4,37 @@ import Bars from './components/bars.jsx';
 import Search from './components/search.jsx';
 import autoBind from 'react-autobind';
 
-class App extends Component {
-    constructor(props) {
+class App extends Component 
+{
+    constructor(props) 
+	{
         super(props);
         autoBind(this);
 
-        this.state = {
-	    data: [],
-	}
+        this.state = { data: [] };
     }
 
     componentDidMount() {}
     
-    setData(data) {
-	//console.log("setting", data)
-	this.setState({data: data})
-	//console.log(this.state.data)
+    setData(data) 
+	{
+		//console.log("setting", data)
+		this.setState({ data: data });
+		//console.log(this.state.data)
     }
 
-    render() {
-	return (
-	    <div className="main">
-		<Search
-		    set={this.setData}
-		/>
-		<div className="over-bars">
-		    <Bars data={this.state.data}/>
-		</div>
-	    </div>
-
-	)
+    render() 
+	{
+		return (
+			<div className="main">
+				<Search
+					set={this.setData}
+				/>
+				<div className="over-bars">
+					<Bars data={this.state.data}/>
+				</div>
+			</div>
+		)
     }
 }
 
--- app_holder/corsaurus/src/components/search.jsx
+++ app_holder/corsaurus/src/components/search.jsx
@@ -3,92 +3,100 @@ import React, { Component } from 'react';
 import autoBind from 'react-autobind';
 //const autoBind = require('auto-bind/react');
 
-class Search extends Component {
+class Search extends Component 
+{
     constructor(props) 
     {
-	super(props);
-	autoBind(this);
+		super(props);
+		autoBind(this);
 
-	this.state = { mounted: false, data: [] };
+		this.state = { mounted: false, data: [] };
     }
 
-    parseString(v) {
-	let pos = []
-	let neg = []
-	let lp = 0
-	let p = "+"
+    parseString(v) 
+	{
+		let pos = [];
+		let neg = [];
+		let lp = 0;
+		let p = "+";
 
-	this.pusher = (i) => {
-	    let mod = v.slice(lp, i).replace(/((?<!\\)[+-\\])+/, "").trim()
-	    if (p == '+') { pos.push(mod) } else { neg.push(mod) }
-	}
-
-	for (let i in v) {
-	    if ((v[i-1] != "\\") && ((v[i] == '-') || (v[i] == '+'))) {
-		if (i != lp) {
-		    this.pusher(i)
-		    lp = i
+		this.pusher = (i) => {
+			let mod = v.slice(lp, i).replace(/((?<!\\)[+-\\])+/, "").trim();
+			if (p == '+') { pos.push(mod) } else { neg.push(mod) }
 		}
-		p = v[i]
-	    }
-	    if (i == v.length - 1) { this.pusher(i+1) }
-	}
-	return [pos, neg]
-    }
 
-    checkText(v) {
-
-    }
-
-    handleTextChange(e) {
-    }
-
-    handleSubmit(e) {
-	if (e.key == "Enter") {
-	    let parsed = this.parseString(e.target.value)
-	    this.makeRequest( 
+		for (let i in v) 
 		{
-		    'num': 100,
-		    'pos': parsed[0],
-		    'neg': parsed[1],
-		    'mode': 'sum'
+			if ((v[i-1] != "\\") && ((v[i] == '-') || (v[i] == '+'))) 
+			{
+				if (i != lp) 
+				{
+					this.pusher(i);
+					lp = i;
+				}
+				p = v[i];
+			}
+			if (i == v.length - 1) { this.pusher(i+1) }
 		}
-	    )
-	}
+		return [pos, neg]
     }
 
-    makeRequest(request) {
-	fetch('http://localhost:5000/query', {
-	    method: 'put',
-	    body: JSON.stringify(request),
-	    headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
-	}).then(res => res.json()).then((data) => {
-	    this.props.set(data)
-	}).catch((err) => console.error(err));
+    checkText(v) {}
+
+    handleTextChange(e) {}
+
+    handleSubmit(e) 
+	{
+		if (e.key == "Enter") 
+		{
+			let parsed = this.parseString(e.target.value);
+			this.makeRequest( 
+			{
+				'num': 100,
+				'pos': parsed[0],
+				'neg': parsed[1],
+				'mode': 'sum'
+			});
+		}
+    }
+
+    makeRequest(request) 
+	{
+		fetch('http://localhost:5000/query', 
+		{
+			method: 'put',
+			body: JSON.stringify(request),
+			headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+		})
+			.then(res => res.json()).then((data) => 
+			{
+				this.props.set(data)
+			})
+			.catch((err) => console.error(err));
     }
 
     componentDidMount() {
-	this.setState({mounted: true})
-	this.makeRequest(
-	    {
-		'num': 100,
-		'pos': ['king', 'woman'],
-		'neg': ['man'],
-		'mode': 'sum'
-	    }
-	)
+		this.setState({mounted: true})
+		this.makeRequest(
+			{
+			'num': 100,
+			'pos': ['king', 'woman'],
+			'neg': ['man'],
+			'mode': 'sum'
+			}
+		)
     }
 
     render() {
-	return (
-	    <div className="search-wrapper">
-		<input className="search-input" 
-		    onChange={this.handleTextChange}
-		    onKeyDown={this.handleSubmit}
-		    placeholder={"king + woman - man"}
-		/>
-	    </div>
-	)
+		return (
+			<div className="search-wrapper">
+			<input className="search-input" 
+				onChange={this.handleTextChange}
+				onKeyDown={this.handleSubmit}
+				placeholder={"king + woman - man"}
+			/>
+			</div>
+		)
     }
 }
 
