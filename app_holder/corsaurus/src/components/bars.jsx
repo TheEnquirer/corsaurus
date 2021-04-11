d4a087ac3c80bcc36c1022498028f7dc095b4d92
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2021-04-11 16:46:15 -0700

    Added loading indicator.

--- app_holder/corsaurus/src/components/bars.jsx
+++ app_holder/corsaurus/src/components/bars.jsx
@@ -12,9 +12,10 @@ class Bars extends Component
 	    autoBind(this);
 
 	    this.state = {
-		data: this.props.data,
-		mounted: false,
-		shown: 1,
+            data: this.props.data,
+            mounted: false,
+            shown: 1,
+            loading: true
 	    };
 
 	    this.more = React.createRef();
@@ -44,50 +45,75 @@ class Bars extends Component
         }
     }
 
-    render() {
-	return (
-	    <div className="bars-wrapper">
-		{(this.state.mounted && this.state.data)? this.state.data.slice(0, 10*this.state.shown).map((item, i) => (
-		    <div className="bar-unit" style={{width: this.wid+620}}>
-			<div className="word-wrapper" style={{width: this.wid}}>
-			    <div className="word">{item[0]}</div>
-			</div>
-			<Spring native to={{width: 500}}>
-			    {props =>
-			    <animated.div className="bottom-gradient" style={{...props}}>&nbsp;</animated.div>
-			    }
-			</Spring>
-			<Spring native to={{width: item[1] * 500}}>
-			    {props =>
-			    <animated.div className="bar-gradient" style={{...props}}>&nbsp;</animated.div>
-			    }
-			</Spring>
-		    </div>
-		)) : "nothing returned :("}
-		<div
-		    ref={this.more}
-		>
-		    <p 
-		    className="arrow"
-		    onClick={() => {
-			console.log(this.state.shown*10, this.state.data.length)
-			if (this.state.shown*10 <= this.state.data.length) { 
-			    this.setState({shown: this.state.shown + 1}, () => {
-				if (this.more) {
-				    this.more.current.scrollIntoView({
-					behavior: "smooth", 
-					block: "start",
-				    })
-				    console.log("scrolling")
-				}
-			    })
-			}
-		    }}
-		>{(this.state.shown*10 <= this.state.data.length)? "▼" : "∅"} </p>
-		</div>
+    moreIndicator() 
+    {
+        if (this.state.data.length == 0) { return ""; }
+        else
+        {
+            if (this.state.loading) { this.setState({ loading: false }); }
+            if (this.state.shown*10 <= this.state.data.length) { return "▼"; }
+            else { return "∅"; }
+        }
+    }
 
-	    </div>
-	)
+    loadingIndicator() 
+    {
+        return this.state.loading && (
+            <div class="loader"></div>
+        );
+    }
+
+    render() 
+    {
+        return (
+            <div>
+                <div className="bars-wrapper">
+                    {(this.state.mounted && this.state.data)? this.state.data.slice(0, 10*this.state.shown).map((item, i) => (
+                        <div className="bar-unit" style={{width: this.wid+620}}>
+                            <div className="word-wrapper" style={{width: this.wid}}>
+                                <div className="word">{item[0]}</div>
+                            </div>
+                            <Spring native to={{width: 500}}>
+                                {props =>
+                                <animated.div className="bottom-gradient" style={{...props}}>&nbsp;</animated.div>
+                                }
+                            </Spring>
+                            <Spring native to={{width: item[1] * 500}}>
+                                {props =>
+                                <animated.div className="bar-gradient" style={{...props}}>&nbsp;</animated.div>
+                                }
+                            </Spring>
+                        </div>
+                    )) : "nothing returned :("}
+                    <div
+                        ref={this.more}
+                    >
+                        <p 
+                            className="arrow"
+                            onClick={() => {
+                            console.log(this.state.shown*10, this.state.data.length)
+                            if (this.state.shown*10 <= this.state.data.length) { 
+                                this.setState({shown: this.state.shown + 1}, () => {
+                                if (this.more) {
+                                    this.more.current.scrollIntoView({
+                                    behavior: "smooth", 
+                                    block: "start",
+                                    })
+                                    console.log("scrolling")
+                                }
+                                })
+                            }
+                            }}
+                        >
+                            {this.moreIndicator()}
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    {this.loadingIndicator()}
+                </div>
+            </div>
+        )
     }
 }
 
